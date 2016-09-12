0c482aa5ba78cfc0a7c6b7a0e3b6efe3de1fc267
Jason L Perry <jasper@ambethia.com>
2016-09-12 10:47:07 -0400

    added fetch request to make a move

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,9 +4,34 @@ import { Cup, Well } from '.'
 
 class Game extends React.Component {
 
+  constructor () {
+    super()
+    this.state = {
+      currentMove: []
+    }
+  }
+
   componentDidMount () {
     window.fetch(`https://sensei-sense-api.herokuapp.com/games?access_token=pickles`, {
       method: 'POST'
+    }).then((response) => response.json())
+    .then((data) => {
+      this.setState({
+        id: data.id,
+        moves: data.moves
+      })
+    })
+  }
+
+  _submitTurn = () => {
+    window.fetch(`https://sensei-sense-api.herokuapp.com/games/move?access_token=pickles&game_id=${this.state.id}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        guess: this.state.currentMove
+      })
     }).then((response) => response.json())
     .then((data) => {
       this.setState({
@@ -35,7 +60,7 @@ class Game extends React.Component {
         <Cup droppable />
         <Cup droppable />
         <div className='go'>
-          <button>Go</button>
+          <button onClick={this._submitTurn}>Go</button>
         </div>
       </div>
       <div className='color-well'>
